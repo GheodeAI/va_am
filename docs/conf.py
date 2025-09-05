c2e89aed77c56d7cac6a4c80665cdd663b71016e
cosminmarina <cosmin.marina@edu.uah.es>
2025-09-05 11:33:16 +0200

    feat: update citation
    - add fa-quote-rigth
    
    - move custom-citation.html to _templates

--- docs/conf.py
+++ docs/conf.py
@@ -80,5 +80,5 @@ html_theme = 'pydata_sphinx_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-# templates_path = ['_templates']
-templates_path = ['.']
+templates_path = ['_templates']
+
